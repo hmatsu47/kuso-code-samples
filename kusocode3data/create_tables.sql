76846411af1589fe5d10064da690e3b09fd38646
hmatsu47 <35142774+hmatsu47@users.noreply.github.com>
2022-02-04 14:03:12 +0900

    クソコード 3 用のテーブル定義が間違っていたので修正

--- kusocode3data/create_tables.sql
+++ kusocode3data/create_tables.sql
@@ -1,13 +1,13 @@
-CREATE DATABASE `picture`;
-CREATE TABLE `picture`.`access_count` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `picture_id` int(11) NOT NULL,
-  `access_count` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 CREATE TABLE `picture`.`count_log` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `picture_id` int(11) NOT NULL,
   `access_count` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1001 DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `picture`.`picture` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(20) NOT NULL,
+  `description` varchar(50) NOT NULL,
+  `image` longblob NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
